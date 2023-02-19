fe9d7afbf63864f8d7bb2e8788d3864dd830468b
wangyw15 <wangyanwei2012@sina.com>
2023-02-19 10:26:55 +0800

    Changed to new weight

--- src/main.m
+++ src/main.m
@@ -18,19 +18,36 @@ cities = table2array(combined(:, 1));
 a = table2array(combined(:, 2:10));
 
 %% calculate weight
-[n, m] = size(a);
+% m for the number of samples, n for the number of factors
+[m, n] = size(a);
+
+% rescale
+for i = 1:n
+    if n == 5 || n == 8
+        a(:,i) = 1 - (a(:,i) - min(a(:,i))) / (max(a(:,i)) - min(a(:,i)));
+    else
+        a(:,i) = (a(:,i) - min(a(:,i))) / (max(a(:,i)) - min(a(:,i)));
+    end
+end
 
 % calculate weight
 p = a ./ sum(a);
-e = -sum(p .* log(p)) / log(n);
-g = 1 - e;
-w = g / sum(g); % this is weight
+
+h = zeros(1, n);
+for i = 1:n
+    for j = 1:m
+        if p(j,i) ~= 0
+            h(i) = h(i) - p(j,i) * log(p(j,i)) / log(n);
+        end
+    end
+end
+
+w = zeros(1, n);
+for i = 1:n
+    w(i) = (sum(h) + 1 - h(i)) / sum(sum(h) + 1 - 2.*h);
+end
 
 %% TOPSIS
-% rescale
-for i = 1:m
-    a(:,i) = rescale(a(:,i));
-end
 
 b = a ./ vecnorm(a);
 c = b .* w;
@@ -38,9 +55,9 @@ Cstar = max(c);
 C0 = min(c);
 
 % the lower the worser
-Cstar(5) = min(c(:, 5)); 
+Cstar(4) = min(c(:, 4)); 
 Cstar(8) = min(c(:, 8));
-C0(5) = max(c(:, 5));
+C0(4) = max(c(:, 4));
 C0(8) = max(c(:, 8));
 
 Sstar = vecnorm(c - Cstar, 2, 2);
